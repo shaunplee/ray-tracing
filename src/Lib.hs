a2d800c286de7554e229994ffeb8e9a99ccf364c
Shaun Lee <shaun.lee@gmail.com>
2020-03-19 07:39:52 -0700

    Change Vec3 to a newtype of a tuple.

--- src/Lib.hs
+++ src/Lib.hs
@@ -22,65 +22,74 @@ type RGB = Vec3 Word8
 
 type XYZ = Vec3 Double
 
-data Vec3 a = Vec3
-  { vecX :: a
-  , vecY :: a
-  , vecZ :: a
-  }
+newtype Vec3 a = Vec3 (a, a, a)
+
+vecX :: Vec3 a -> a
+vecX (Vec3 (x, _ , _)) = x
+
+vecY :: Vec3 a -> a
+vecY (Vec3 (_, y, _)) = y
+
+vecZ :: Vec3 a -> a
+vecZ (Vec3 (_, _, z)) = z
 
 instance Functor Vec3 where
-  fmap f (Vec3 x y z) = Vec3 (f x) (f y) (f z)
+  fmap f (Vec3 (x, y, z)) = Vec3 ((f x), (f y), (f z))
 
 instance Show a => Show (Vec3 a) where
-  show (Vec3 x y z) = show x ++ " " ++ show y ++ " " ++ show z
+  show (Vec3 (x, y, z)) = show x ++ " " ++ show y ++ " " ++ show z
 
 instance (Floating a, Num a) => Num (Vec3 a) where
-  (+) (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = Vec3 (x1 + x2) (y1 + y2) (z1 + z2)
-  (-) (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = Vec3 (x1 - x2) (y1 - y2) (z1 - z2)
-  (*) (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = Vec3 (x1 * x2) (y1 * y2) (z1 * z2)
-  negate (Vec3 x y z) = Vec3 (-x) (-y) (-z)
+  (+) (Vec3 (x1, y1, z1)) (Vec3 (x2, y2, z2)) =
+    Vec3 ((x1 + x2), (y1 + y2), (z1 + z2))
+  (-) (Vec3 (x1, y1, z1)) (Vec3 (x2, y2, z2)) =
+    Vec3 ((x1 - x2), (y1 - y2), (z1 - z2))
+  (*) (Vec3 (x1, y1, z1)) (Vec3 (x2, y2, z2)) =
+    Vec3 ((x1 * x2), (y1 * y2), (z1 * z2))
+  negate (Vec3 (x, y, z)) = Vec3 ((-x), (-y), (-z))
   --this definition of abs is clearly wrong, as it should be a scalar
   --not a vector
-  abs (Vec3 x y z) = Vec3 (sqrt (x * x + y * y + z * z)) 0 0
-  signum v@(Vec3 x y z) =
-    let (Vec3 m _ _) = abs v
-     in Vec3 (x / m) (y / m) (z / m)
-  fromInteger x = Vec3 (fromInteger x) 0 0
+  abs (Vec3 (x, y, z)) = Vec3 ((sqrt (x * x + y * y + z * z)), 0, 0)
+  signum v@(Vec3 (x, y, z)) =
+    let (Vec3 (m, _, _)) = abs v
+     in Vec3 ((x / m), (y / m), (z / m))
+  fromInteger x = Vec3 ((fromInteger x), 0, 0)
 
 vecDiv :: Fractional a => Vec3 a -> Vec3 a -> Vec3 a
-vecDiv (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = Vec3 (x1 / x2) (y1 / y2) (z1 / z2)
+vecDiv (Vec3 (x1, y1, z1)) (Vec3 (x2, y2, z2)) =
+  Vec3 ((x1 / x2), (y1 / y2), (z1 / z2))
 
 length :: (Floating a, Num a) => Vec3 a -> a
-length v = let (Vec3 l _ _) = abs v in l
+length v = let (Vec3 (l, _, _)) = abs v in l
 
 squaredLength :: (Num a) => Vec3 a -> a
-squaredLength (Vec3 x y z) = x * x + y * y + z * z
+squaredLength (Vec3 (x, y, z)) = x * x + y * y + z * z
 
 makeUnitVector :: (Floating a) => Vec3 a -> Vec3 a
 makeUnitVector = signum
 
 scale :: (Floating a) => a -> Vec3 a -> Vec3 a
-scale k (Vec3 x y z) = Vec3 (k * x) (k * y) (k * z)
+scale k (Vec3 (x, y, z)) = Vec3 ((k * x), (k * y), (k * z))
 
 divide :: (Floating a) => Vec3 a -> a -> Vec3 a
-divide (Vec3 x y z) k = Vec3 (x / k) (y / k) (z / k)
+divide (Vec3 (x, y, z)) k = Vec3 ((x / k), (y / k), (z / k))
 
 dot :: Num a => Vec3 a -> Vec3 a -> a
-dot (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = x1 * x2 + y1 * y2 + z1 * z2
+dot (Vec3 (x1, y1, z1)) (Vec3 (x2, y2, z2)) = x1 * x2 + y1 * y2 + z1 * z2
 
 cross :: Num a => Vec3 a -> Vec3 a -> Vec3 a
-cross (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) =
-  Vec3 (y1 * z2 - z1 * y2) (z1 * x2 - x1 * z2) (x1 * y2 - y1 * x2)
+cross (Vec3 (x1, y1, z1)) (Vec3 (x2, y2, z2)) =
+  Vec3 ((y1 * z2 - z1 * y2), (z1 * x2 - x1 * z2), (x1 * y2 - y1 * x2))
 
 
 r :: RGB -> Word8
-r (Vec3 x _ _) = x
+r (Vec3 (x, _, _)) = x
 
 g :: RGB -> Word8
-g (Vec3 _ y _) = y
+g (Vec3 (_, y, _)) = y
 
 b :: RGB -> Word8
-b (Vec3 _ _ z) = z
+b (Vec3 (_, _, z)) = z
 
 scaleColor :: Double -> Word8
 scaleColor x = floor (255.99 * sqrt x)
@@ -170,7 +179,7 @@ randomInUnitSphere = do
   x <- cRandomDoubleIO
   y <- cRandomDoubleIO
   z <- cRandomDoubleIO
-  let p = scale 2.0 (Vec3 x y z) - Vec3 1.0 1.0 1.0
+  let p = scale 2.0 (Vec3 (x, y, z)) - Vec3 (1.0, 1.0, 1.0)
   if squaredLength p < 1.0 then return p else randomInUnitSphere
 
 data Camera = Camera
@@ -188,10 +197,10 @@ getRay c u v =
      camera_origin c)
 
 defaultCamera :: Camera
-defaultCamera = let lowerLeftCorner = Vec3 (-2.0) (-1.0) (-1.0)
-                    horizontal      = Vec3 4.0 0.0 0.0
-                    vertical        = Vec3 0.0 2.0 0.0
-                    origin          = Vec3 0.0 0.0 0.0
+defaultCamera = let lowerLeftCorner = Vec3 ((-2.0), (-1.0), (-1.0))
+                    horizontal      = Vec3 (4.0, 0.0, 0.0)
+                    vertical        = Vec3 (0.0, 2.0, 0.0)
+                    origin          = Vec3 (0.0, 0.0, 0.0)
                  in Camera origin lowerLeftCorner horizontal vertical
 
 color :: Hittable a => Ray -> [a] -> IO (Vec3 Double)
@@ -206,7 +215,8 @@ color r htbls =
       let unitDirection = makeUnitVector (direction r)
           t = 0.5 * (vecY unitDirection + 1.0)
        in return $
-          scale (1.0 - t) (Vec3 1.0 1.0 1.0) + scale t (Vec3 0.5 0.7 1.0)
+          scale (1.0 - t) (Vec3 (1.0, 1.0, 1.0)) +
+          scale t (Vec3 (0.5, 0.7, 1.0))
 
 pixelPositions :: Int -> Int -> [[(Int, Int)]]
 pixelPositions nx ny = map (\y -> map (, y) [0 .. nx - 1]) [ny - 1,ny - 2 .. 0]
@@ -220,8 +230,9 @@ someFunc = do
   putStrLn $ show nx ++ " " ++ show ny
   putStrLn "255"
   let world =
-        [ Sphere (Vec3 0.0 0.0 (-1.0)) 0.5
-        , Sphere (Vec3 0.0 (-100.5) (-1.0)) 100]
+        [ Sphere (Vec3 (0.0, 0.0, (-1.0))) 0.5
+        , Sphere (Vec3 (0.0, (-100.5), (-1.0))) 100
+        ]
   let cam = defaultCamera
   let pp = pixelPositions nx ny
   let sampleColor :: (Int, Int) -> Vec3 Double -> Int -> IO (Vec3 Double)
@@ -235,7 +246,8 @@ someFunc = do
         return $ accCol + c1
   let renderPos :: (Int, Int) -> IO RGB
       renderPos (x, y) = do
-        summedColor <- foldM (sampleColor (x, y)) (Vec3 0.0 0.0 0.0) [0..ns-1]
+        summedColor <-
+          foldM (sampleColor (x, y)) (Vec3 (0.0, 0.0, 0.0)) [0 .. ns - 1]
         return $ scaleColors $ divide summedColor (fromIntegral ns)
   vals <- mapM (mapM renderPos) pp
   mapM_ printRow vals
